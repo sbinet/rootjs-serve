54454a8d391f2080011f6bbf72097a7ae20cfde4
Sebastien Binet <binet@cern.ch>
2016-10-14 17:18:42 +0200

    main: add -browser flag

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 var (
 	addrFlag = flag.String("addr", ":5555", "server address:port")
 	dirFlag  = flag.String("dir", ".", "directory of files to serve")
+	browser  = flag.String("browser", "", "path to browser to launch if not empty")
 )
 
 func main() {
@@ -18,7 +21,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = http.ListenAndServe(*addrFlag, http.FileServer(http.Dir(dir)))
+
+	if *browser != "" {
+		go func() {
+			log.Printf("launching browser at localhost%s/...\n", *addrFlag)
+			cmd := exec.Command(*browser, "localhost"+*addrFlag+"/")
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
+	serve(dir)
+}
+
+func serve(dir string) {
+	log.Printf("serving at %q...\n", *addrFlag)
+	err := http.ListenAndServe(*addrFlag, http.FileServer(http.Dir(dir)))
 	if err != nil {
 		log.Fatal(err)
 	}
